c7c0ef5ec3f53156827a6c212df1f9f621537e9e
realtimecoder <gtannaverma@gmail.com>
2026-09-05 16:36:01 +0530

    gov hub

--- core/urls.py
+++ core/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('how-it-works/', views.how_it_works, name='how_it_works'),
+    path('government-opportunities/', views.government_opportunities, name='government_opportunities'),
+    path('government-opportunities/apply/<int:project_id>/', views.apply_government_opportunity, name='apply_government_opportunity'),
     path('contact/', views.contact, name='contact'),
     path('pricing-policy/', views.pricing_policy, name='pricing_policy'),
 ]
